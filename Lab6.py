a7a817703be9cb96f1f7f95b3d271804cc3a44a4
houricky <148251802+houricky@users.noreply.github.com>
2023-10-26 18:00:16 -0400

    Add files via upload

---
+++ Lab6.py
@@ -0,0 +1,30 @@
+# Ricky Hou
+def encode(orig_pass):
+    result = ''
+    for numbers in orig_pass:
+        new_numbers = str((int(numbers) + 3) % 10)
+        result += new_numbers
+    return result
+
+
+def main():
+    testing = True
+    while testing:
+        print("Menu")
+        print("-------------")
+        print("1. Encode\n2. Decode\n3. Quit\n")
+        option = int(input("Please enter an option:"))
+        if option == 1:
+            orig_pass = input("Please enter your password to encode:")
+            orig_pass = encode(orig_pass)
+            print("Your password has been encoded and stored!\n")
+
+        elif option == 2:
+            print(f"The encoded password is {orig_pass}, and the original password is .")
+
+        else:
+            testing = False
+
+
+if __name__ == "__main__":
+    main()
